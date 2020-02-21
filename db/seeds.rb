eb9847ba8fbd89d09d5237081147821d6dda1f4b
Iram Shehzadi <Shehzadi193@gmail.com>
2020-02-21 15:24:15 +0000

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'json'
+require 'open-uri'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,7 +10,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Cocktail.create(name: 'mojito')
-Ingredient.create(name: 'lemon')
-Ingredient.create(name: 'ice')
-Ingredient.create(name: 'mint leaves')
+# Cocktail.create(name: 'mojito')
+# Ingredient.create(name: 'lemon')
+# Ingredient.create(name: 'ice')
+# Ingredient.create(name: 'mint leaves')
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients = JSON.parse(open(url).read)
+
+puts 'Destroying!'
+Ingredient.destroy_all
+ingredients['drinks'].each do |hash|
+  Ingredient.create(name: hash['strIngredient1'])
+end
